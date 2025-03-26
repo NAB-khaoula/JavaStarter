e6d572c0e1a550dc7dfe0ee99b5889f81a801c78
Naboussi, Khaoula (Naas) <Khaoula.Naboussi@sgs.com>
2025-03-26 13:38:11 +0000

    variables

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        System.out.println("Hello world!");
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What is your name");
+        String name = scan.nextLine();
+
+        System.out.println("how old are you");
+        int age = scan.nextInt();
+        scan.nextLine();
+        System.out.println("What is your favorite food");
+        String food = scan.nextLine();
+
+        System.out.println("welcome "+ name);
+        System.out.println("your are  "+ age + " years old");
+        System.out.println("your like "+ food);
+
     }
 }
